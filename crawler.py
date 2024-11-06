11e74503d2ca5e3043d4b7adfdb08054dadf2ac4
khalmatay <khalmatay@gmail.com>
2024-11-06 18:17:01 +0100

    finished crawler.py

--- crawler.py
+++ crawler.py
@@ -5,48 +5,62 @@ from bs4 import BeautifulSoup
 
 CONST_URL = "https://guide.michelin.com/en/it/restaurants"
 
+
 def get_html(url):
     response = requests.get(url)
-    response.raise_for_status()  # проверка на ошибки
+    response.raise_for_status()
     return response.content
 
 
-r = requests.get(CONST_URL)
-soup = BeautifulSoup(r.content, "html.parser")
-
-restaurant_urls = []
-page = 1
-
-last_page_link = soup.select(
-    "div.js-restaurant__bottom-pagination ul.pagination a.btn.btn-outline-secondary.btn-sm")
-max_pages = int(last_page_link[-2].get_text().strip())
-
-print(max_pages)
-
-while page <= max_pages:
-    print("Number", page)
-    url = f"{CONST_URL}/page/{page}"
-    html = get_html(url)
+def get_number_of_max_pages():
+    html = get_html(CONST_URL)
     soup = BeautifulSoup(html, "html.parser")
-    print(url)
+    last_page_link = soup.select(
+        "div.js-restaurant__bottom-pagination ul.pagination a.btn.btn-outline-secondary.btn-sm")
+    max_pages = int(last_page_link[-2].get_text().strip())
+    return max_pages
 
-    restaurant_list = soup.select("div.row.restaurant__list-row.js-restaurant__list_items a.link")
 
-    if not restaurant_list:
-        break
+def get_restaurant_urls(max_pages):
+    restaurant_urls = []
+    page = 1
+    while page <= max_pages:
+        url = f"{CONST_URL}/page/{page}"
+        html = get_html(url)
+        soup = BeautifulSoup(html, "html.parser")
 
-    for link in restaurant_list:
-        restaurant_url = link["href"]
-        if restaurant_url:
-            full_url = f"https://guide.michelin.com{restaurant_url}"
-            restaurant_urls.append(full_url)
+        print(page, url)
 
-    page += 1
-    time.sleep(1)
 
-print(len(set(restaurant_urls)))
-with open('restaurant_urls.txt', 'w') as f:
-    for url in restaurant_urls:
-        f.write(url + "\n")
+        restaurant_list = soup.select("div.row.restaurant__list-row.js-restaurant__list_items a.link")
 
-print(f"Saved {len(restaurant_urls)} URL into restaurant_urls.txt")
+        if not restaurant_list:
+            break
+
+        for link in restaurant_list:
+            restaurant_url = link["href"]
+            if restaurant_url:
+                full_url = f"https://guide.michelin.com{restaurant_url}"
+                restaurant_urls.append(full_url)
+
+        page += 1
+        time.sleep(1)
+    print("Unique restaurant urls", len(set(restaurant_urls)))
+    return restaurant_urls
+
+
+def save_urls_to_file(restaurant_urls):
+    with open('restaurant_urls.txt', 'w') as f:
+        for url in restaurant_urls:
+            f.write(url + "\n")
+    print(f"Saved {len(restaurant_urls)} URL into restaurant_urls.txt")
+
+
+def crawler():
+    max_pages = get_number_of_max_pages()
+    restaurant_urls = get_restaurant_urls(max_pages)
+    save_urls_to_file(restaurant_urls)
+
+
+if __name__ == "__main__":
+    crawler()
